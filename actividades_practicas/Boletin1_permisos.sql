758be9b2c51a24c55491bdc9abe36f70ac81a60f
mgarsan719 <margagon154@gmail.com>
2025-12-15 10:51:51 +0100

    Create Boletin1_permisos.sql

---
+++ actividades_practicas/Boletin1_permisos.sql
@@ -0,0 +1,16 @@
+-- ACT 1
+-- show grants; -- Entrando como ROOT
+
+-- ACT 2
+-- create user Administrador identified by 'pass_admin';
+-- grant all privileges on *.* to Administrador;
+
+-- ACT 3
+-- hecho
+
+-- ACT 4
+-- select user from mysql.user; -- Entradno como Administrador
+-- select user(); -- Entradno como Administrador
+
+-- ACT 5
+show grants; -- Entrando como Administrador!
